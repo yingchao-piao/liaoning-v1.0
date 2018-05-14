48c84fcd0d4a21c399fe669dc6bb0d9b1c167da2
cnic-escience <pyc@cnic.cn>
2018-05-14 16:59:19 +0800

    Bing BaseMap

--- public/javascripts/map/layerCtrl.js
+++ public/javascripts/map/layerCtrl.js
@@ -5,37 +5,16 @@
  * Time: 14:38
  * Description: OpenLayer加载Geoserver发布的图层
 */
-
 var geoserverUrl = 'http://localhost:8080/geoserver/';
 
 //geoserver发布的工作空间、图层的名称
 var layersAttr = {
     workspace:'liaoning',
     layerNames:{
+        bingaerial:'bingaerial',
         xbm:'J2210000JB2006XBM_utf8'
     }
 };
-
-var styles = [
-    'Road',
-    'RoadOnDemand',
-    'Aerial',
-    'AerialWithLabels',
-    'collinsBart',
-    'ordnanceSurvey'
-];
-var layers = [];
-var i, ii;
-for (i = 0, ii = styles.length; i < ii; ++i) {
-    layers.push(new ol.layer.Tile({
-        visible: false,
-        preload: Infinity,
-        source: new ol.source.BingMaps({
-            key: 'AttVbOas-R25-sOVvUkfWipFTx7ryIlHJh0ryn7O9Q07h8a-XpFf0DSY0qtH6whp',
-            imagerySet: styles[i]
-        })
-    }));
-}
 
 //openlayers各图层source
 var mapLayerSource={
@@ -52,22 +31,27 @@ var mapLayerSource={
     }
 };
 
-//构建mapModule对象，先初始化图层的相关属性
+//构建地图模块，设置相关属性
 var mapModule = {
+    baseMap: new ol.layer.Tile({
+        preload: Infinity,
+        source: new ol.source.BingMaps({
+            key: 'AttVbOas-R25-sOVvUkfWipFTx7ryIlHJh0ryn7O9Q07h8a-XpFf0DSY0qtH6whp',
+            imagerySet: 'AerialWithLabels'
+        })
+    }),
     xbm_wms_layer: new ol.layer.Tile({
         source: mapLayerSource.baseLayerSource.xbm_wms_source
     }),
     defaultView:new ol.View({
-        center: ol.proj.transform([122.312,41.096], 'EPSG:4326', 'EPSG:3857'),
-        zoom:6.5
+        projection: 'EPSG:4326',
+        center: [122.312,41.096],
+        zoom:7
     })
 };
 
-layers.push(mapModule.xbm_wms_layer);
-
 var map = new ol.Map({
     target: 'map',
-    layers: layers,
-    loadTilesWhileInteracting: true,
+    layers: [mapModule.baseMap, mapModule.xbm_wms_layer],
     view: mapModule.defaultView
 });

adb9af66c4858f15f514d7df52172646ea3610b7
cnic-escience <pyc@cnic.cn>
2018-05-23 10:44:16 +0800

    20180523

--- public/javascripts/map/layerCtrl.js
+++ public/javascripts/map/layerCtrl.js
@@ -33,6 +33,7 @@ var mapLayerSource={
 
 //构建地图模块，设置相关属性
 var mapModule = {
+    //Bing在线底图
     baseMap: new ol.layer.Tile({
         preload: Infinity,
         source: new ol.source.BingMaps({
@@ -40,9 +41,13 @@ var mapModule = {
             imagerySet: 'AerialWithLabels'
         })
     }),
+
+    //林相图
     xbm_wms_layer: new ol.layer.Tile({
         source: mapLayerSource.baseLayerSource.xbm_wms_source
     }),
+
+    //默认的图层显示样式
     defaultView:new ol.View({
         projection: 'EPSG:4326',
         center: [122.312,41.096],
@@ -54,4 +59,28 @@ var map = new ol.Map({
     target: 'map',
     layers: [mapModule.baseMap, mapModule.xbm_wms_layer],
     view: mapModule.defaultView
+});
+
+//点击地图查看相关feature信息
+map.on('singleclick', function(evt) {
+    document.getElementById('info').innerHTML = '';
+    var viewResolution = /** @type {number} */ (mapModule.defaultView.getResolution());
+    var url = mapLayerSource.baseLayerSource.xbm_wms_source.getGetFeatureInfoUrl(
+        evt.coordinate, viewResolution, 'EPSG:4326',
+        {'INFO_FORMAT': 'text/html'});
+    if (url) {
+        document.getElementById('info').innerHTML =
+            '<iframe seamless src="' + url + '"></iframe>';
+    }
+});
+
+map.on('pointermove', function(evt) {
+    if (evt.dragging) {
+        return;
+    }
+    var pixel = map.getEventPixel(evt.originalEvent);
+    var hit = map.forEachLayerAtPixel(pixel, function() {
+        return true;
+    });
+    map.getTargetElement().style.cursor = hit ? 'pointer' : '';
 });
